787e9d11ed701fd91a69dfa6f255d1849b091636
Seungwon_Lee <swsw.lee@linecorp.com>
2020-02-21 13:46:59 +0900

    strange blur

--- app/src/main/java/com/example/seungwon/openglestriangle/blur/BlurRenderer.kt
+++ app/src/main/java/com/example/seungwon/openglestriangle/blur/BlurRenderer.kt
@@ -3,6 +3,7 @@ package com.example.seungwon.openglestriangle.blur
 import android.content.Context
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
+import android.opengl.Matrix
 import com.example.seungwon.openglestriangle.ProgramInfo
 import com.example.seungwon.openglestriangle.R
 import com.example.seungwon.openglestriangle.util.FrameBufferUtil
@@ -15,17 +16,36 @@ import javax.microedition.khronos.opengles.GL10
 
 class BlurRenderer(private val context: Context) : GLSurfaceView.Renderer {
     private val squareCoords: FloatArray = floatArrayOf(
-        // X, Y
-        1f, 1f, -1f, 1f, -1f, -1f,
-        -1f, -1f, 1f, -1f, 1f, 1f
+        -1.0f, -1.0f,   // 0 bottom left
+        1.0f, -1.0f,   // 1 bottom right
+        -1.0f, 1.0f,   // 2 top left
+        1.0f, 1.0f   // 3 top right
+    )
+    private val txtCoords: FloatArray = floatArrayOf(
+        0.0f, 0.0f,     // 0 bottom left
+        1.0f, 0.0f,     // 1 bottom right
+        0.0f, 1.0f,     // 2 top left
+        1.0f, 1.0f    // 3 top right
     )
     private val vertexBuffer: FloatBuffer
+    private val txtBuffer: FloatBuffer
 
     private var passThroughProgram: Int = 0
     private var guassianVerticalProgram: Int = 0
     private var guassianHorizontalProgram: Int = 0
 
-    private var bogumTextureId: Int = 0
+    private var textureId: Int = 0
+    private var positionHandle: Int = 0
+    private var mvpHandle: Int = 0
+    private var textureHandle: Int = 0
+    private var textureMvpHandle: Int = 0
+    private var textureCoordsHandle: Int = 0
+
+    private var texelWidthOffset: Int = 0
+    private var texelHeightOffset: Int = 0
+
+    private var width: Int = 0
+    private var height: Int = 0
 
     private val textureFrameBuffers: ArrayList<FrameBufferUtil.TextureFrameBuffer> = ArrayList()
 
@@ -36,83 +56,80 @@ class BlurRenderer(private val context: Context) : GLSurfaceView.Renderer {
                 .asFloatBuffer()
                 .put(squareCoords)
         vertexBuffer.position(0)
+
+        txtBuffer = ByteBuffer.allocateDirect(txtCoords.size * FLOAT_BYTE_SIZE)
+            .order(ByteOrder.nativeOrder())
+            .asFloatBuffer()
+        txtBuffer.put(txtCoords)
     }
 
     override fun onDrawFrame(gl: GL10?) {
-        //render scene
-//        GLES20.glUseProgram(passThroughProgram)
-//        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, SQUARE_LINE_COUNT)
+        GLES20.glClearColor(0f, 0f, 0f, 1f)
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
 
-        //render scene to FBO A, using horizontal blur
-        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, textureFrameBuffers[0].frameBufferId) // TODO 30
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, bogumTextureId)
-        GLES20.glUseProgram(guassianHorizontalProgram)
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, SQUARE_LINE_COUNT)
-        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0) // TODO 30
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId)
 
-        //render FBO B to scene, using vertical blur
-        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, textureFrameBuffers[1].frameBufferId) // TODO 30
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureFrameBuffers[0].textureId)
-        GLES20.glUseProgram(guassianVerticalProgram)
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, SQUARE_LINE_COUNT)
-        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0) // TODO 30
+        val identityMatrix = FloatArray(16)
+        Matrix.setIdentityM(identityMatrix, 0)
+        GLES20.glUniformMatrix4fv(mvpHandle, 1, false, identityMatrix, 0)
+        GLES20.glUniformMatrix4fv(textureMvpHandle, 1, false, identityMatrix, 0)
 
-        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0) // TODO 30
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureFrameBuffers[0].textureId)
-        GLES20.glUseProgram(passThroughProgram)
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, SQUARE_LINE_COUNT)
+        GLES20.glEnableVertexAttribArray(positionHandle)
+        GLES20.glVertexAttribPointer(
+            positionHandle, X_Y_COORDS_NUMBER, GLES20.GL_FLOAT, false,
+            X_Y_COORDS_NUMBER * FLOAT_BYTE_SIZE, vertexBuffer
+        )
+        GLES20.glEnableVertexAttribArray(textureCoordsHandle)
+        GLES20.glVertexAttribPointer(
+            textureCoordsHandle,
+            TEXTURE_COORDS_VERTEX_NUMBER,
+            GLES20.GL_FLOAT,
+            false,
+            TEXTURE_COORDS_VERTEX_NUMBER * FLOAT_BYTE_SIZE,
+            txtBuffer
+        )
+
+        GLES20.glDrawArrays(
+            GLES20.GL_TRIANGLE_STRIP,
+            0,
+            squareCoords.size / X_Y_COORDS_NUMBER
+        )
+
+        GLES20.glDisableVertexAttribArray(positionHandle)
+        GLES20.glDisableVertexAttribArray(textureCoordsHandle)
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
-
+        this.width = width
+        this.height = height
+        GLES20.glViewport(0, 0, width, height)
     }
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
         // load shaders
-        passThroughProgram = ProgramInfo.createProgramV30(
+        passThroughProgram = ProgramInfo.createProgram(
             context,
             R.raw.blur_pass_through_vertex_shader,
             R.raw.blur_pass_through_fragment_shader
         )
-        guassianVerticalProgram = ProgramInfo.createProgramV30(
-            context,
-            R.raw.blur_pass_through_vertex_shader,
-            R.raw.blur_gaussian_linear_vertex_fragment_shader
-        )
-        guassianHorizontalProgram = ProgramInfo.createProgramV30(
-            context,
-            R.raw.blur_pass_through_vertex_shader,
-            R.raw.blur_gaussian_linear_horiz_fragment_shader
-        )
+        GLES20.glUseProgram(passThroughProgram)
 
-        // create temporary framebuffer
-        textureFrameBuffers.add(FrameBufferUtil.createFrameTextureBuffer())
-        textureFrameBuffers.add(FrameBufferUtil.createFrameTextureBuffer())
+        positionHandle = GLES20.glGetAttribLocation(passThroughProgram, "aPosition")
+        textureCoordsHandle = GLES20.glGetAttribLocation(passThroughProgram, "aTextureCoord")
 
-        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0) // TODO 30
+        textureHandle = GLES20.glGetUniformLocation(passThroughProgram, "uTexture")
 
-        // load a texture of the target image
+        mvpHandle = GLES20.glGetUniformLocation(passThroughProgram, "uMVPMatrix")
+        textureMvpHandle = GLES20.glGetUniformLocation(passThroughProgram, "uTexMatrix")
+
+        texelWidthOffset = GLES20.glGetUniformLocation(passThroughProgram, "uTexelWidthOffset")
+        texelHeightOffset = GLES20.glGetUniformLocation(passThroughProgram, "uTexelHeightOffset")
+
         val bitmap = TxtLoaderUtil.getBitmap(context, R.drawable.bogum)
-        bogumTextureId = TxtLoaderUtil.getTxt(bitmap)
+        textureId = TxtLoaderUtil.getTxt(bitmap)
 
-        // create full-screen quad mesh
-        val bufferIds = IntArray(1)
-        GLES20.glGenBuffers(1, bufferIds, 0)
-        GLES20.glBufferData(
-            GLES20.GL_ARRAY_BUFFER,
-            X_Y_COORDS_NUMBER * FLOAT_BYTE_SIZE * SQUARE_X_Y_COUNT,
-            vertexBuffer,
-            GLES20.GL_STATIC_DRAW
-        )
-        GLES20.glEnableVertexAttribArray(0)
-        GLES20.glVertexAttribPointer(
-            0,
-            X_Y_COORDS_NUMBER,
-            GLES20.GL_FLOAT,
-            false,
-            FLOAT_BYTE_SIZE * X_Y_COORDS_NUMBER,
-            0
-        )
+//        GLES20.glUniform1f(texelHeightOffset, if (width == 0) 0f else BLUR_RATIO / height)
+        GLES20.glUniform1f(texelWidthOffset, /*if (width == 0) 0f else BLUR_RATIO / width*/0f)
     }
 
     companion object {
@@ -122,5 +139,8 @@ class BlurRenderer(private val context: Context) : GLSurfaceView.Renderer {
         private const val FLOAT_BYTE_SIZE = 4
         private const val SQUARE_X_Y_COUNT = 6
         private const val SQUARE_LINE_COUNT = 6
+        private const val TEXTURE_COORDS_VERTEX_NUMBER = 2
+
+        private const val BLUR_RATIO = 0.1f
     }
 }
--- app/src/main/res/raw/blur_pass_through_vertex_shader.glsl
+++ app/src/main/res/raw/blur_pass_through_vertex_shader.glsl
@@ -1,7 +1,32 @@
+#define SAMPLES 9
 precision mediump float;
 
-in vec2 VertexPosition;
+uniform mat4 uMVPMatrix;  // MVP의 변형 매트릭스 (전체 변형)
+uniform mat4 uTexMatrix;  // 텍스처 변형 매트릭스 (텍스처 변형 만 해당)
 
-void main(void) {
-    gl_Position = vec4(VertexPosition, 0.0, 1.0);
+uniform float uTexelWidthOffset;
+uniform float uTexelHeightOffset;
+
+attribute vec4 aPosition;
+attribute vec4 aTextureCoord;
+
+varying vec2 vTextureCoord;
+varying vec2 vBlurTextureCoord[SAMPLES];
+
+
+void main() {
+    gl_Position = uMVPMatrix * aPosition;
+    vTextureCoord = (uTexMatrix * aTextureCoord).xy;
+
+    int multiplier = 0;
+    vec2 blurStep;
+    vec2 offset = vec2(uTexelHeightOffset, uTexelWidthOffset);
+
+    for (int i = 0; i < SAMPLES; i++)
+    {
+        multiplier = (i - ((SAMPLES-1) / 2));
+        // ToneCurve in x (horizontal)
+        blurStep = float(multiplier) * offset;
+        vBlurTextureCoord[i] = vTextureCoord + blurStep;
+    }
 }
