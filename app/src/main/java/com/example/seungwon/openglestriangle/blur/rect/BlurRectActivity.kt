d59dd4ad21e1ecff5b0b4aa8041486ece08c3853
Seung won Lee (Ellen) <l901120@gmail.com>
2020-03-03 16:07:18 +0900

    added ontouch rect blur rendering

--- app/src/main/java/com/example/seungwon/openglestriangle/blur/rect/BlurRectActivity.kt
+++ app/src/main/java/com/example/seungwon/openglestriangle/blur/rect/BlurRectActivity.kt
@@ -1,15 +1,18 @@
 package com.example.seungwon.openglestriangle.blur.rect
 
-import android.graphics.PixelFormat
 import android.opengl.GLSurfaceView
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import android.widget.ImageView
 import com.example.seungwon.openglestriangle.R
 
-class BlurRectActivity : AppCompatActivity() {
+class BlurRectActivity : AppCompatActivity(), View.OnTouchListener {
     private var glSurfaceView: GLSurfaceView? = null
+    private val renderer: BlurRectRenderer = BlurRectRenderer(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +21,9 @@ class BlurRectActivity : AppCompatActivity() {
         glSurfaceView = findViewById(R.id.gl_surface)
         glSurfaceView?.let {
             it.setEGLContextClientVersion(2)
-//            it.setZOrderOnTop(true)
-//            it.setEGLConfigChooser(8, 8, 8, 8, 16, 0)
-//            it.holder.setFormat(PixelFormat.RGBA_8888)
-            it.setRenderer(BlurRectRenderer(this))
-//            it.renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
+            it.setRenderer(renderer)
+            it.renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
+            it.setOnTouchListener(this)
         }
         val imageView = findViewById<ImageView>(R.id.blur_rect_img_view)
         imageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.park_dotori))
@@ -36,5 +37,32 @@ class BlurRectActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         glSurfaceView?.onPause()
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        if (event == null) {
+            return false
+        }
+        Log.d("BlurRectActivity", "onTouch ${event.action} ${event.x} ${event.y}")
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                renderer.rectStartPointX = event.x
+                renderer.rectStartPointY = event.y
+                return true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                return true
+            }
+            MotionEvent.ACTION_UP -> {
+                glSurfaceView?.queueEvent {
+                    renderer.onDrawBlurRect(
+                        event.x,
+                        event.y
+                    ) { glSurfaceView?.requestRender() }
+                }
+                return true
+            }
+        }
+        return false
     }
 }
--- app/src/main/java/com/example/seungwon/openglestriangle/blur/rect/BlurRectRenderer.kt
+++ app/src/main/java/com/example/seungwon/openglestriangle/blur/rect/BlurRectRenderer.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.opengl.Matrix
+import android.util.Log
 import com.example.seungwon.openglestriangle.ProgramInfo
 import com.example.seungwon.openglestriangle.R
 import com.example.seungwon.openglestriangle.util.FrameBufferUtil
@@ -13,6 +14,7 @@ import java.nio.ByteOrder
 import java.nio.FloatBuffer
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
+
 
 class BlurRectRenderer(private val context: Context) : GLSurfaceView.Renderer {
     private val squareCoords: FloatArray = floatArrayOf(
@@ -29,20 +31,11 @@ class BlurRectRenderer(private val context: Context) : GLSurfaceView.Renderer {
         1f, 0f,
         0f, 0f
     )
-    private val rectSquareCoords: FloatArray = floatArrayOf(
-        -0.5f, -0.5f,   // 0 bottom left
-        0.5f, -0.5f,   // 1 bottom right
-        -0.5f, 0.5f,   // 2 top left
-        0.5f, 0.5f   // 3 top right
-    )
-    private val rectTxtCoords: FloatArray = floatArrayOf(
-        0.25f, 0.75f,     // 2 top left
-        0.75f, 0.75f,    // 3 top right
-        0.25f, 0.25f,     // 0 bottom left
-        0.75f, 0.25f     // 1 bottom right
-    )
+
     private val vertexBuffer: FloatBuffer
     private val txtBuffer: FloatBuffer
+    private val rectVertexCoordsList: ArrayList<FloatArray> = ArrayList()
+    private val rectTxtCoordsList: ArrayList<FloatArray> = ArrayList()
     private val rectVertexBuffer: FloatBuffer
     private val rectTxtBuffer: FloatBuffer
 
@@ -70,6 +63,11 @@ class BlurRectRenderer(private val context: Context) : GLSurfaceView.Renderer {
     private var width: Int = 0
     private var height: Int = 0
 
+    @Volatile
+    var rectStartPointX: Float = 0f
+    @Volatile
+    var rectStartPointY: Float = 0f
+
     private val textureFrameBuffers: ArrayList<FrameBufferUtil.TextureFrameBuffer> = ArrayList()
 
     init {
@@ -86,18 +84,13 @@ class BlurRectRenderer(private val context: Context) : GLSurfaceView.Renderer {
             .put(txtCoords)
         txtBuffer.position(0)
 
-        rectVertexBuffer =
-            ByteBuffer.allocateDirect(rectSquareCoords.size * FLOAT_BYTE_SIZE)
-                .order(ByteOrder.nativeOrder())
-                .asFloatBuffer()
-                .put(rectSquareCoords)
-        rectVertexBuffer.position(0)
-
-        rectTxtBuffer = ByteBuffer.allocateDirect(rectTxtCoords.size * FLOAT_BYTE_SIZE)
+        rectVertexBuffer = ByteBuffer.allocateDirect(txtCoords.size * FLOAT_BYTE_SIZE)
             .order(ByteOrder.nativeOrder())
             .asFloatBuffer()
-            .put(rectTxtCoords)
-        rectTxtBuffer.position(0)
+
+        rectTxtBuffer = ByteBuffer.allocateDirect(txtCoords.size * FLOAT_BYTE_SIZE)
+            .order(ByteOrder.nativeOrder())
+            .asFloatBuffer()
     }
 
     override fun onDrawFrame(gl: GL10?) {
@@ -105,7 +98,18 @@ class BlurRectRenderer(private val context: Context) : GLSurfaceView.Renderer {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
 
         renderOriginalTexture()
-        renderBlurredTexture()
+
+        rectVertexCoordsList.forEachIndexed { index, floats ->
+            rectVertexBuffer.clear()
+            rectVertexBuffer.put(floats)
+            rectVertexBuffer.position(0)
+
+            rectTxtBuffer.clear()
+            rectTxtBuffer.put(rectTxtCoordsList[index])
+            rectTxtBuffer.position(0)
+
+            renderBlurredTexture(rectVertexBuffer, rectTxtBuffer)
+        }
     }
 
     private fun renderOriginalTexture() {
@@ -134,7 +138,10 @@ class BlurRectRenderer(private val context: Context) : GLSurfaceView.Renderer {
         GLES20.glDisableVertexAttribArray(originalTextureCoordsHandle)
     }
 
-    private fun renderBlurredTexture() {
+    private fun renderBlurredTexture(
+        rectVertexBuffer: FloatBuffer,
+        rectTxtBuffer: FloatBuffer
+    ) {
         Matrix.setIdentityM(matrixView, 0)
 
         GLES20.glUseProgram(passThroughProgram)
@@ -160,9 +167,9 @@ class BlurRectRenderer(private val context: Context) : GLSurfaceView.Renderer {
         )
 
         GLES20.glDrawArrays(
-            GLES20.GL_TRIANGLE_STRIP,
+            GLES20.GL_TRIANGLE_FAN,
             0,
-            rectSquareCoords.size / X_Y_COORDS_NUMBER
+            squareCoords.size / X_Y_COORDS_NUMBER
         )
 
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0)
@@ -188,7 +195,8 @@ class BlurRectRenderer(private val context: Context) : GLSurfaceView.Renderer {
         val originalBitmap = TxtLoaderUtil.getBitmap(context, R.drawable.park_dotori)
         originalTextureId = TxtLoaderUtil.getTxt(originalBitmap)
 
-        originalPositionHandle = GLES20.glGetAttribLocation(originalRenderingProgram, "vPosition")
+        originalPositionHandle =
+            GLES20.glGetAttribLocation(originalRenderingProgram, "vPosition")
         originalTextureCoordsHandle =
             GLES20.glGetAttribLocation(originalRenderingProgram, "a_texCoord")
         originalMvpHandle = GLES20.glGetUniformLocation(originalRenderingProgram, "uMVPMatrix")
@@ -211,18 +219,129 @@ class BlurRectRenderer(private val context: Context) : GLSurfaceView.Renderer {
         textureMvpHandle = GLES20.glGetUniformLocation(passThroughProgram, "uTexMatrix")
 
         texelWidthOffset = GLES20.glGetUniformLocation(passThroughProgram, "uTexelWidthOffset")
-        texelHeightOffset = GLES20.glGetUniformLocation(passThroughProgram, "uTexelHeightOffset")
+        texelHeightOffset =
+            GLES20.glGetUniformLocation(passThroughProgram, "uTexelHeightOffset")
 
         val bitmap = TxtLoaderUtil.getBitmap(context, R.drawable.park_dotori)
-        val bitmapWidth = bitmap.width / 2
+        val bitmapWidth = VERTEX_RESOLUTION
         textureId = TxtLoaderUtil.getTxt(bitmap)
 
 //        GLES20.glUniform1f(texelHeightOffset, if (width == 0) 0f else BLUR_RATIO / height)
         GLES20.glUniform1f(
             texelWidthOffset,
-            BLUR_RATIO / bitmapWidth.toFloat()
+            BLUR_RATIO / bitmapWidth
         )
     }
+
+    fun onDrawBlurRect(endPointX: Float, endPointY: Float, postAction: () -> Unit): Boolean {
+        Log.d(TAG, "onTouch")
+        if (width == 0 || height == 0) {
+            Log.e(TAG, "onTouch $width $height == 0")
+            return false
+        }
+
+        val rectLengthPixelX = (endPointX - rectStartPointX)
+        val rectLengthPixelY = (endPointY - rectStartPointY)
+
+        Log.d(TAG, "onTouch rectStartPointX $rectStartPointX rectStartPointY $rectStartPointY")
+        Log.d(TAG, "onTouch rectLengthPixelX $rectLengthPixelX rectLengthPixelY $rectLengthPixelY")
+
+        val texturePointX = rectStartPointX / width
+        val texturePointY = rectStartPointY / height
+        val rectangleTextureX = texturePointX + (rectLengthPixelX / width)
+        val rectangleTextureY = texturePointY + (rectLengthPixelY / height)
+        val textureCoords = FloatArray(8)
+
+//        0f, 1f,
+//        1f, 1f,
+//        1f, 0f,
+//        0f, 0f
+
+        textureCoords[0] = texturePointX
+        textureCoords[1] = rectangleTextureY
+        textureCoords[2] = rectangleTextureX
+        textureCoords[3] = rectangleTextureY
+        textureCoords[4] = rectangleTextureX
+        textureCoords[5] = texturePointY
+        textureCoords[6] = texturePointX
+        textureCoords[7] = texturePointY
+
+//        textureCoords[0] = 0f
+//        textureCoords[1] = 1f
+//        textureCoords[2] = 1f
+//        textureCoords[3] = 1f
+//        textureCoords[4] = 1f
+//        textureCoords[5] = 0f
+//        textureCoords[6] = 0f
+//        textureCoords[7] = 0f
+
+        textureCoords.forEach {
+            Log.d(TAG, "foreach $it ")
+        }
+
+        rectTxtCoordsList.add(textureCoords)
+
+        val startPointX = getNomalizedGlCoordsX(rectStartPointX, width)
+        val startPointY = getNomalizedGlCoordsY(rectStartPointY, height)
+        var rectX = getNomalizedGlCoordsX(endPointX, width)
+        var rectY = getNomalizedGlCoordsY(endPointY, height)
+
+        if (rectX > 1) {
+            rectX = 1f
+        }
+        if (rectX < -1) {
+            rectX = -1f
+        }
+
+        if (rectY > 1) {
+            rectY = 1f
+        }
+        if (rectY < -1) {
+            rectY = -1f
+        }
+
+        Log.d(TAG, "onTouch rectX $rectX rectY $rectY")
+
+//        private val squareCoords: FloatArray = floatArrayOf(
+//            // X, Y
+//            -1f, -1f,
+//            1f, -1f,
+//            1f, 1f,
+//            -1f, 1f
+//        )
+        val vertextCoords = FloatArray(8)
+        vertextCoords[0] = startPointX
+        vertextCoords[1] = rectY
+        vertextCoords[2] = rectX
+        vertextCoords[3] = rectY
+        vertextCoords[4] = rectX
+        vertextCoords[5] = startPointY
+        vertextCoords[6] = startPointX
+        vertextCoords[7] = startPointY
+
+//        vertextCoords[0] = -1f
+//        vertextCoords[1] = -1f
+//        vertextCoords[2] = 1f
+//        vertextCoords[3] = -1f
+//        vertextCoords[4] = 1f
+//        vertextCoords[5] = 1f
+//        vertextCoords[6] = -1f
+//        vertextCoords[7] = 1f
+
+        rectVertexCoordsList.add(vertextCoords)
+
+        postAction.invoke()
+
+        return true
+    }
+
+    private fun getNoalizedGlTextureCoordsY(y: Float): Float = (1 - y)
+
+    private fun getNomalizedGlCoordsX(x: Float, screenWidth: Int): Float =
+        x / screenWidth * 2.0f - 1.0f
+
+    private fun getNomalizedGlCoordsY(y: Float, screenHeight: Int): Float =
+        y / screenHeight * -2.0f + 1.0f
 
     companion object {
         private const val TAG = "BlurRectRenderer"
@@ -232,6 +351,8 @@ class BlurRectRenderer(private val context: Context) : GLSurfaceView.Renderer {
         private const val SQUARE_X_Y_COUNT = 6
         private const val SQUARE_LINE_COUNT = 6
         private const val TEXTURE_COORDS_VERTEX_NUMBER = 2
+        private const val VERTEX_RESOLUTION = 300f
+        private const val TEXTURE_RESOLUTION = 0.5f
 
         private const val BLUR_RATIO = 1f
     }
